1caf8f9ad71799f5d036833d948c903ce8799a9d
Guillermo Cámbara Ruiz <guillermocambara@gmail.com>
2021-10-25 12:34:08 +0200

    Changed the WER computation backend to jiwer to get S, D and I

--- data/utils/logs/fairseq_scores_to_tsv.py
+++ data/utils/logs/fairseq_scores_to_tsv.py
@@ -8,9 +8,9 @@ WER scores shall be appended at such file, and output in a new one.
 
 import os
 import argparse
+import jiwer
 import numpy as np
 import pandas as pd
-from fairseq import scoring
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description="Compute WER scores from fairseq log.") 
@@ -20,9 +20,6 @@ parser.add_argument("--dst", default="", help="destination tsv file with the fai
 args = parser.parse_args()
 print("Arguments")
 print(args)
-
-print("Building fairseq WER scorer...")
-scorer = scoring.build_scorer('wer', None)
 
 if args.src_tsv == "":
     print("No base .tsv file provided in --src_tsv argument, results shall be stored in a new .tsv file.")
@@ -47,20 +44,30 @@ print("Scoring utterances with WER...")
 if args.src_tsv == "":
     scores_list = []
     for sample_id, (ref, hyp) in eval_dict.items():
-        scorer.add_string(ref, hyp)
-        wer = scorer.result_string().split('WER: ')[1]
-        scorer.reset()
+        measures = jiwer.compute_measures(ref, hyp)
+        wer = measures['wer']*100.0
+        s = measures['substitutions']
+        d = measures['deletions']
+        i = measures['insertions']
+        n = len(ref.split(' '))
 
-        scores_list.append([sample_id, ref, hyp, wer])
-    df = pd.DataFrame(scores_list, columns=['id', 'ref', 'hyp', 'wer'])
+        scores_list.append([sample_id, ref, hyp, wer, s, d, i, n])
+    df = pd.DataFrame(scores_list, columns=['id', 'ref', 'hyp', 'wer', 'substitutions', 'deletions', 'insertions', 'n_words'])
 else:
     df['wer'] = np.nan
     for sample_id, (ref, hyp) in eval_dict.items():
-        scorer.add_string(ref, hyp)
-        wer = scorer.result_string().split('WER: ')[1]
-        scorer.reset()
+        measures = jiwer.compute_measures(ref, hyp)
+        wer = measures['wer']*100.0
+        s = measures['substitutions']
+        d = measures['deletions']
+        i = measures['insertions']
+        n = len(ref.split(' '))
 
         assert (ref == df.loc[int(sample_id), 'tgt_text']), "The reference text indicated by the sample ID in the transcripts file does not match with the one stored in the dataset!"
         df.at[int(sample_id), 'wer'] = wer
+        df.at[int(sample_id), 'substitutions'] = s
+        df.at[int(sample_id), 'deletions'] = d
+        df.at[int(sample_id), 'insertions'] = i
+        df.at[int(sample_id), 'n_words'] = n
 
 df.to_csv(args.dst, sep='\t', index=None)
