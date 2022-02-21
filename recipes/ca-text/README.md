5bf81864d67d9222aa56769e55359fabbbe77979
Guillermo Cambara <guillermocambara@gmail.com>
2022-02-21 19:05:04 +0100

    Scanning of all txt files in ca-text is supported now

--- recipes/ca-text/README.md
+++ recipes/ca-text/README.md
@@ -10,5 +10,20 @@ git clone https://github.com/Softcatala/ca-text-corpus.git
 To generate a file containing each unique word from a text corpus, in a single line, run the following command:
 ```
 SRC=/path/to/ca-text-corpus/data/common-short-sentences.txt
-python get_words.py --src $SRC --dst ./words.txt --tokens ./tokens.txt --tokens_format other
+python get_words.py --src $SRC --dst ./words.txt --tokens ./tokens.txt --tokens_format other --cat_tokens ./tokens_cat.txt
+```
+
+With ```tokens_cat.txt``` it is ensured that no strange characters are kept in the words file.
+
+Also, you can submit more than one text file, separating the paths with commas:
+```
+SRC1=/path/to/ca-text-corpus/data/common-short-sentences.txt
+SRC2=/path/to/ca-text-corpus/data/common-voice-sentences.txt
+python get_words.py --src $SRC1,$SRC2 --dst ./words.txt --tokens ./tokens.txt --tokens_format other --cat_tokens ./tokens_cat.txt
+```
+
+If you want to get the words from all the txt files, simply input the path to the directory containing those, and all will be taken:
+```
+SRC_DIR=/path/to/ca-text-corpus/data
+python get_words.py --src $SRC_DIR --dst ./words.txt --tokens ./tokens.txt --tokens_format other --cat_tokens ./tokens_cat.txt
 ```
--- recipes/ca-text/get_words.py
+++ recipes/ca-text/get_words.py
@@ -2,22 +2,42 @@ import argparse
 import os
 from tqdm import tqdm
 
-def transform(word):
+def transform(word, cat_tokens):
     word = word.replace('.', '')
     word = word.replace('!', '')
     word = word.replace(',', '')
     word = word.replace('?', '')
     word = word.replace(':', '')
+    word = word.replace(';', '')
     word = word.replace('»', '')
     word = word.replace('«', '')
     word = word.replace('–', '')
     word = word.replace('…', '')
+    word = word.replace('"', '')
+    word = word.replace('$', '')
+    word = word.replace('%', '')
+    word = word.replace('*', '')
+    word = word.replace('>', '')
+    word = word.replace('<', '')
+    word = word.replace('~', '')
+    word = word.replace('“', '')
+    word = word.replace('”', '')
     word = word.lower()
+
+    trailing_tokens = "'-·"
+    word = word.strip(trailing_tokens)
+
+    if cat_tokens:
+        for token in word:
+            if token not in cat_tokens:
+                word = ''
+
     return word
 
 parser = argparse.ArgumentParser(description="Reads corpus txt files and generates a file with each unique word per line.") 
-parser.add_argument("--src", default="", help="path to each corpus txt file, separated by commas", required=True)
+parser.add_argument("--src", default="", help="path to each corpus txt file, separated by commas. Otherwise, especify the directory, and all sub txt files will be taken", required=True)
 parser.add_argument("--dst", default="", help="destination location to store words file, including the name of the file itself", required=True)
+parser.add_argument("--cat_tokens", default="./tokens_cat.txt", help="file that contains predefined catalan tokens. After preprocessing, if a word contains a token that is not included here, it will be discarded", required=False)
 parser.add_argument("--tokens", default="", help="destination directory to store tokens file", required=False)
 parser.add_argument("--tokens_format", default="other", help="format of the tokens file: wav2letter | other", required=False)
 args = parser.parse_args()
@@ -27,13 +47,26 @@ print(args)
 lexicon = {}
 tokens = {}
 
-txt_files = args.src.split(',')
+if args.cat_tokens != "":
+    with open(args.cat_tokens, 'r') as f:
+        cat_tokens = f.read().splitlines() 
+else:
+    cat_tokens = None
+
+if os.path.isdir(args.src):
+    txt_files = []
+    for root, dirs, files in os.walk(args.src):
+        for file in files:
+            if file.endswith(".txt"):
+                txt_files.append(os.path.join(root, file))
+else:
+    txt_files = args.src.split(',')
 
 for txt_file in tqdm(txt_files):
     print(f"Reading words in {txt_file}...")
     with open(txt_file, 'r') as f:
         lines = f.readlines()
-        for line in lines:
+        for line in tqdm(lines):
             words = line.strip().split(' ')
             for word in words:
                 if word == " ":
@@ -41,8 +74,11 @@ for txt_file in tqdm(txt_files):
                 elif word == "":
                     continue
                 else:
-                    word = transform(word)
-                    lexicon[word] = " ".join(word)
+                    word = transform(word, cat_tokens=cat_tokens)
+                    if word == "":
+                        continue
+                    else:
+                        lexicon[word] = " ".join(word)
 
                 for char in word:
                     tokens[char] = 0
